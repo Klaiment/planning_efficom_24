bae9f39d6325f24f7d857e654f6ad52414fdf7c5
Clement <clement62ing@gmail.com>
2024-03-05 10:46:40 +0100

    structuration by "Tisma"

--- app/main.py
+++ app/main.py
@@ -9,7 +9,6 @@ class Entreprise(BaseModel):
     ville: str
     code_postal: str
     pays: str
-
 class User(BaseModel):
     id: int
     nom: str
@@ -18,32 +17,28 @@ class User(BaseModel):
     password: str
     role: str
     entreprise_id: int
-
 class Planning(BaseModel):
     id: int
     date_start: str
     date_end: str
     nom: str
     entreprise_id: int
-
 class UserPlanning(BaseModel):
     id: int
     user_id: int
     planning_id: int
-
 class Notification(BaseModel):
     id: int
     id_user: int
     id_planning: int
     title: str
     details: str
-
 class NotificationVue(BaseModel):
     id: int
     user_id: int
     notif_id: int
     date: str
-
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
