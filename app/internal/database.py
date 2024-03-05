2fe8c100623289b4dc62163274420c7dea811b85
Clement <clement62ing@gmail.com>
2024-03-05 16:36:35 +0100

    fix exec & query func

--- app/internal/database.py
+++ app/internal/database.py
@@ -13,7 +13,14 @@ try:
         cursor = connection.cursor()
         cursor.execute(query)
         result = cursor.fetchall()
+        connection.close()
         return result
+    def execute(query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        connection.commit()
+        connection.close()
+        return 1
 
 except mariadb.Error as err:
     print(f"Erreur lors de la connexion à la base de données : {err}")
