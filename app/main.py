334b82e41ec0b47deaeca6ebafaae8596f697d11
Clement <clement62ing@gmail.com>
2024-03-07 12:11:01 +0100

    feat: Ajout des routes pour la gestion des user_task

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from routers.entreprises import router as entreprise_router
 from routers.users import router as user_router
 from internal.auth import router as auth_router
+from routers.planning import router as planning_router
+from routers.notification import router as notification_router
 from internal.database import query
 app = FastAPI()
 # Vérification de la connexion à la base de données
@@ -9,4 +12,7 @@ async def root():
     return {"message": "Hello World"}
 
 app.include_router(user_router, tags=["Users"])
+app.include_router(entreprise_router, tags=["Entreprises"])
 app.include_router(auth_router, tags=["Users"])
+app.include_router(planning_router, tags=["Plannings"])
+app.include_router(notification_router, tags=["Notifications"])
---
+++ app/routers/notification.py
@@ -0,0 +1,48 @@
+from fastapi import APIRouter, HTTPException, status, Depends
+from pydantic import BaseModel
+from models.notification import Notification
+from internal.database import query, execute
+from internal.auth import get_decoded_token
+
+router = APIRouter()
+
+# Affiche toutes les notifications
+@router.get("/notifications")
+async def get_notifications(connected_user_email: str = Depends(get_decoded_token)):
+    notifications = query("SELECT * FROM notification")
+    return {"notifications": notifications}
+
+# Crée une notification
+@router.post("/notification/")
+async def create_notification(notification: Notification, connected_user_email: str = Depends(get_decoded_token)):
+    req = f'INSERT INTO notification (id_planning, id_user, details, title) VALUES ("{notification.planning_id}", "{notification.user_id}", "{notification.detail}", "{notification.title}")'
+    execute(req)
+    return {"message": "Notification created"}
+
+# Affiche une notification
+@router.get("/notification/{notification_id}")
+async def get_notification(notification_id: int, connected_user_email: str = Depends(get_decoded_token)):
+    notification = query(f"SELECT * FROM notification WHERE id={notification_id}")
+    if len(notification) == 0:
+        raise HTTPException(status_code=404, detail="Notification not found")
+    return {"notification": notification}
+
+# Met à jour une notification
+@router.put("/notification/{notification_id}")
+async def update_notification(notification_id: int, notification: Notification, connected_user_email: str = Depends(get_decoded_token)):
+    check_notification = query(f"SELECT * FROM notification WHERE id={notification_id}")
+    if len(check_notification) == 0:
+        raise HTTPException(status_code=404, detail="Notification not found")
+    req = f'UPDATE notification SET id_planning="{notification.planning_id}", id_user="{notification.user_id}", details="{notification.detail}", title="{notification.title}" WHERE id={notification_id}'
+    execute(req)
+    return {"message": "Notification updated"}
+
+# Supprime une notification
+@router.delete("/notification/{notification_id}")
+async def delete_notification(notification_id: int, connected_user_email: str = Depends(get_decoded_token)):
+    check_notification = query(f"SELECT * FROM notification WHERE id={notification_id}")
+    if len(check_notification) == 0:
+        raise HTTPException(status_code=404, detail="Notification not found")
+    req = f'DELETE FROM notification WHERE id={notification_id}'
+    execute(req)
+    return {"message": "Notification deleted"}
