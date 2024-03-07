138051612e263153197870e7a41e93f58f55eb43
Clement <clement62ing@gmail.com>
2024-03-07 16:15:24 +0100

    feat: Ajout de la conformité RGPD en cryptant les données sensibles

--- app/routers/users.py
+++ app/routers/users.py
@@ -16,8 +16,10 @@ router = APIRouter()
 @router.get("/users")
 async def get_users(connected_user_email: Annotated[str, Depends(get_decoded_token)]):
     users = query("SELECT * FROM user")
+    if len(users) == 0:
+        raise HTTPException(status_code=404, detail="No users found")
     return {"users": users}
-@router.post("/utilisateur/")
+@router.post("/user/")
 async def create_user(user: User):
     check_user = query(f"SELECT * FROM user")
     for unitUser in check_user:
@@ -25,6 +27,9 @@ async def create_user(user: User):
         email_dechiffre = chipher_check.decrypt(unitUser[3]).decode()
         if email_dechiffre == user.email:
             raise HTTPException(status_code=409, detail="User already exists")
+    check_entreprise = query(f"SELECT * FROM entreprise WHERE id={user.entreprise_id}")
+    if len(check_entreprise) == 0:
+        raise HTTPException(status_code=404, detail="Entreprise not found")
     password = password_hash(user.password)
     secret = Fernet.generate_key()
     cipher_suite = Fernet(secret)
@@ -38,8 +43,7 @@ async def create_user(user: User):
     execute(req)
     return {"message": "Create user"}
 
-
-@router.get("/users/{user_id}")
+@router.get("/user/{user_id}")
 async def get_user(connected_user_email: Annotated[str, Depends(get_decoded_token)], user_id: int):
     check_user = query(f"SELECT * FROM user WHERE id={user_id}")
     if len(check_user) == 0:
@@ -66,7 +70,7 @@ async def update_user(connected_user_email: Annotated[str, Depends(get_decoded_t
     if result == 0:
         raise HTTPException(status_code=500, detail="Internal server error")
     return {"message": user}
-@router.delete("/users/{user_id}")
+@router.delete("/user/{user_id}")
 async def delete_user(connected_user_email: Annotated[str, Depends(get_decoded_token)],user_id: int):
     check_user = query(f"SELECT * FROM user WHERE id={user_id}")
     if len(check_user) == 0:
@@ -76,3 +80,51 @@ async def delete_user(connected_user_email: Annotated[str, Depends(get_decoded_t
     if result == 0:
         raise HTTPException(status_code=500, detail="Internal server error")
     return {"message": "Delete user"}
+
+# Routes planning
+@router.get("/user/{user_id}/usertask", tags=["Users/plannings"])
+async def get_plannings(connected_user_email: Annotated[str, Depends(get_decoded_token)], user_id: int):
+    plannings = query(f"SELECT * FROM user_task WHERE user_id={user_id}")
+    if len(plannings) == 0:
+        raise HTTPException(status_code=404, detail="No plannings found for this user")
+    json = []
+    for planning in plannings:
+        json.append({
+            "id": planning[0],
+            "user_id": planning[1],
+            "task_id": planning[2]
+        })
+    return {"plannings": json}
+@router.get("/user/{user_id}/usertask/{user_task}", tags=["Users/plannings"], description="jaime les moches")
+async def get_planning(connected_user_email: Annotated[str, Depends(get_decoded_token)], user_id: int, user_task: int):
+    planning = query(f"SELECT * FROM user_task WHERE id={user_task} AND user_id={user_id}")
+    if len(planning) == 0:
+        raise HTTPException(status_code=404, detail="Planning not found")
+    return {"planning": planning}
+@router.post("/user/{user_id}/planning/{task_id}", tags=["Users/plannings"])
+async def create_planning(connected_user_email: Annotated[str, Depends(get_decoded_token)], user_id: int, task_id: int):
+    check_user = query(f"SELECT * FROM user WHERE id={user_id}")
+    if len(check_user) == 0:
+        raise HTTPException(status_code=404, detail="User not found")
+    check_task = query(f"SELECT * FROM task WHERE id={task_id}")
+    if len(check_task) == 0:
+        raise HTTPException(status_code=404, detail="Task not found")
+    if len(query(f"SELECT * FROM user_task WHERE user_id={user_id} AND task_id={task_id}")) != 0:
+        raise HTTPException(status_code=400, detail="Planning already exists")
+    req = f'INSERT INTO user_task (user_id, task_id) VALUES ("{user_id}","{task_id}")'
+    execute(req)
+    return {"message": "Create planning"}
+@router.delete("/user/{user_id}/planning/{task_id}", tags=["Users/plannings"])
+async def delete_planning(connected_user_email: Annotated[str, Depends(get_decoded_token)], user_id: int, task_id: int):
+    check_user = query(f"SELECT * FROM user WHERE id={user_id}")
+    if len(check_user) == 0:
+        raise HTTPException(status_code=404, detail="User not found")
+    check_planning = query(f"SELECT * FROM user_task WHERE task_id={task_id} AND user_id={user_id}")
+    if len(check_planning) == 0:
+        raise HTTPException(status_code=404, detail="Planning not found")
+    planning_id = check_planning[0][0]
+    req = f'DELETE FROM user_task WHERE id={planning_id}'
+    result = execute(req)
+    if result == 0:
+        raise HTTPException(status_code=500, detail="Internal server error")
+    return {"message": "Delete planning"}
